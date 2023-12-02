590e065373ef813ae753745d7e28ed77f82be2e9
Meet Shah <meetshah7199@gmail.com>
2023-12-01 20:42:22 -0500

    Code for getting data from database, create table using bootstrap and load data from db via Employee Resource to Employee Service to controller to model to thymeleaf to web

---
+++ src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.luv2code.springboot.cruddemo.controller;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService theEmployeeService) {
+        this.employeeService = theEmployeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployee(Model theModel) {
+        //Get the employees from db via Employee Service and Employee Repository
+        List<Employee> theEmployee = employeeService.findAll();
+
+        //Add into model
+        theModel.addAttribute("employees", theEmployee);
+
+        return "list-employees";
+    }
+}
--- src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -31,8 +31,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         if (result.isPresent()) {
             theEmployee = result.get();
-        }
-        else {
+        } else {
             // we didn't find the employee
             throw new RuntimeException("Did not find employee id - " + theId);
         }
